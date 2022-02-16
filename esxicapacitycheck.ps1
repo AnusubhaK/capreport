22c4d193cb9df55848aa2712173a7a89cb6a2352
AnusubhaK <90177553+AnusubhaK@users.noreply.github.com>
2022-02-16 13:35:02 +0530

    Create esxicapacitycheck.ps1

---
+++ esxicapacitycheck.ps1
@@ -0,0 +1,35 @@
+
+#Get list of all clusters
+$Clusterlist = Get-Cluster
+
+#Loop through each Cluster
+$Output = foreach ($Cluster in $Clusterlist)
+{
+
+    $ClusterName = $Cluster.Name
+
+    #Get host and loop through each of them
+    $hostlist=Get-Cluster -Name $ClusterName | Get-VMHost
+    foreach ($hostobj in $hostlist)
+    {
+        $FreeDisk = (Get-Datastore -VMHost $hostobj | Where-Object {$_.Extensiondata.Summary.MultipleHostAccess -eq $True} | Select FreeSpaceGB) 
+        $TotalCPUMhz = Get-VMHost $hostobj | Select CpuTotalMhz 
+        $UsedCPUMhz = Get-VMHost $hostobj | Select CpuUsageMhz 
+        $NumCPU = Get-VMHost $hostobj | Select NumCpu
+        $FreeCpuCore = [math]::Round((($TotalCPUMhz.CpuTotalMhz - $UsedCPUMhz.CpuUsageMhz)/1000)/($TotalCPUMhz.CpuTotalMhz/(1000*$NumCPU.NumCpu)),0)
+        $TotalMemoryGB = Get-VMHost $hostobj | Select MemoryTotalGB 
+        $UsedMemoryGB = Get-VMHost $hostobj | Select MemoryUsageGB 
+        $FreeMemoryGB = [math]::Round(($TotalMemoryGB.MemoryTotalGB - $UsedMemoryGB.MemoryUsageGB),0)
+
+        #Write-Host $ClusterName, "`t" $hostobj.Name, "`t" $FreeDisk.FreeSpaceGB, $TotalCPU.NumCpu, $FreeMemoryGB
+        New-Object -TypeName PSObject -Property @{
+            ClusterName = $ClusterName
+            HostName = $hostobj.Name
+            FreeCPUCore = $FreeCpuCore
+            FreeMemoryGB = $FreeMemoryGB
+            FreeSpaceGB = $FreeDisk.FreeSpaceGB
+        }
+        Select-Object ClusterName, HostName, FreeCPUCore, FreeMemoryGB, FreeSpaceGB
+    }
+}
+$Output | Export-Csv C:\GPoutput.csv -Append
